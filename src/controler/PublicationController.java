d8586fb5ca29c48626cdab8e787faa0681fa8351
leoBello <leobello73@gmail.com>
2018-05-13 13:09:55 +0200

    update

--- src/controler/PublicationController.java
+++ src/controler/PublicationController.java
@@ -1,11 +1,20 @@
 package controler;
 
 
+import contenu.Contenu;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import users.Utilisateurs;
 
-public class PublicationController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PublicationController implements Initializable{
+
+    private Utilisateurs userSession;
+    private Contenu contenu;
 
     @FXML
     private Button jaime;
@@ -20,8 +29,9 @@ public class PublicationController {
     @FXML
     private Button commenter;
 
-    public void like() {
 
+    public void like() {
+        this.userSession.liker(contenu);
     }
 
     public void printComment(){
@@ -40,5 +50,17 @@ public class PublicationController {
     }
 
 
-
+    /**
+     * Called to initialize a controller after its root element has been
+     * completely processed.
+     *
+     * @param location  The location used to resolve relative paths for the root object, or
+     *                  <tt>null</tt> if the location is not known.
+     * @param resources The resources used to localize the root object, or <tt>null</tt> if
+     */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        this.userSession = getCurrentUser();
+        this.contenu = getContenu();
+    }
 }
